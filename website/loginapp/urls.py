a62242db6a40c82881c88e8c535a38013dc09bcf
Ashutos Lenka <ashutoslenka426@gmail.com>
2019-05-16 20:05:26 +0530

    First Commit

--- website/loginapp/urls.py
+++ website/loginapp/urls.py
@@ -6,6 +6,7 @@ app_name = 'loginapp'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
     path('success/', views.success, name='success'),
     path('successlogin/', views.successlogin, name='successlogin'),
     path('signup/', views.UserFormView.as_view(), name='signup'),
